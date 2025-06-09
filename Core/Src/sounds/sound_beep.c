15e97c4c28745690134a9b9fa5bb6c275949c645
Willibot <willert@delta-j.de>
2025-06-09 11:24:53 +0000

    Refactor sound beep functions to use piezo_beep and piezo_stop for improved sound control

--- Core/Src/sounds/sound_beep.c
+++ Core/Src/sounds/sound_beep.c
@@ -19,19 +19,20 @@
 
 #include "sound_beep.h"
 #include "piezo_driver.h"
+extern uint32_t timer_tick; // <-- hinzufügen
 
 static bool beep_active = false;
 static uint32_t beep_end_time = 0;
 
 void sound_beep_start(uint16_t duration_ms, uint16_t frequency_hz) {
-    piezo_on(frequency_hz);
+    piezo_beep(frequency_hz, duration_ms); // <-- piezo_beep statt piezo_on
     beep_end_time = timer_tick + duration_ms;
     beep_active = true;
 }
 
 void sound_beep_update(void) {
     if (beep_active && timer_tick >= beep_end_time) {
-        piezo_off();
+        piezo_stop(); // <-- piezo_stop statt piezo_off
         beep_active = false;
     }
 }
