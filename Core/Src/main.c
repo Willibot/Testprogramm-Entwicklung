dc51106dceae60b4fb34f8eac4a8e505b15de954
Willibot <willert@delta-j.de>
2025-06-28 17:35:56 +0000

    Route EXTI1 explicitly to Port A for testing purposes

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -90,6 +90,8 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  // Test: EXTI1 explizit auf Port A routen (nur zum Test)
+  SYSCFG->EXTICR[0] &= ~(0xF << 4); // Bits 7:4 auf 0 für EXTI1 -> PA1
   MX_DMA_Init();
   MX_TIM3_Init();
   MX_TIM14_Init();
