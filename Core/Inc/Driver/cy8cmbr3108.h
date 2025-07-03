6532b1e7fb6ac50a2d1f97b5579d97cfaba8f0d2
Willibot <willert@delta-j.de>
2025-07-03 19:20:44 +0000

    Update sensor status reading to use input status function

--- Core/Inc/Driver/cy8cmbr3108.h
+++ Core/Inc/Driver/cy8cmbr3108.h
@@ -5,5 +5,7 @@
 
 #define CY8CMBR3108_I2C_ADDR      (0x37 << 1) // Standard-Adresse, ggf. anpassen!
 #define CY8CMBR3108_SENSOR_STATUS 0x00        // Sensor Status Register
+#define CY8CMBR3108_SENSOR_INPUT_STATUS 0x10
 
 uint8_t cy8cmbr3108_read_sensor_status(void);
+uint8_t cy8cmbr3108_read_sensor_input_status(void);
--- Core/Src/Driver/cy8cmbr3108.c
+++ Core/Src/Driver/cy8cmbr3108.c
@@ -3,11 +3,11 @@
 
 extern I2C_HandleTypeDef hi2c1; // Passe ggf. den Handle-Namen an
 
-uint8_t cy8cmbr3108_read_sensor_status(void) {
+uint8_t cy8cmbr3108_read_sensor_input_status(void) {
     uint8_t status = 0;
     HAL_I2C_Mem_Read(&hi2c1,
                      CY8CMBR3108_I2C_ADDR,
-                     CY8CMBR3108_SENSOR_STATUS,
+                     CY8CMBR3108_SENSOR_INPUT_STATUS,
                      I2C_MEMADD_SIZE_8BIT,
                      &status,
                      1,
