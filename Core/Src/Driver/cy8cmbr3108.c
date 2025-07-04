16c794eb2d5737618781fb82dc0f64a79fbe211f
Willibot <willert@delta-j.de>
2025-07-04 19:09:36 +0000

    Update sensor input status definition to match I2C read implementation

--- Core/Src/Driver/cy8cmbr3108.c
+++ Core/Src/Driver/cy8cmbr3108.c
@@ -7,7 +7,7 @@ uint8_t cy8cmbr3108_read_sensor_input_status(void) {
     uint8_t status = 0;
     HAL_I2C_Mem_Read(&hi2c1,
                      CY8CMBR3108_I2C_ADDR,
-                     CY8CMBR3108_SENSOR_INPUT_STATUS,
+                     0x08, // <- Muss jetzt 0x08 sein!
                      I2C_MEMADD_SIZE_8BIT,
                      &status,
                      1,
