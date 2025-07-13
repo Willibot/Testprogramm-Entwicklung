52922deac83ff48d15184a24de82538bfdc25ed5
Willibot <willert@delta-j.de>
2025-07-13 16:49:48 +0000

    Add button status reading functions and improve sensor input status implementation

--- Core/Inc/Driver/cy8cmbr3108.h
+++ Core/Inc/Driver/cy8cmbr3108.h
@@ -5,5 +5,9 @@
 
 #define CY8CMBR3108_I2C_ADDR      (0x37 << 1) // Standard-Adresse
 #define CY8CMBR3108_SENSOR_INPUT_STATUS 0x08  // Sensor Input Status Register
+#define CY8CMBR3108_BUTTON_STAT         0xAA  // Button Status Register
+#define CY8CMBR3108_LATCHED_BUTTON_STAT 0xAC  // Latched Button Status Register
 
 uint8_t cy8cmbr3108_read_sensor_input_status(void);
+uint8_t cy8cmbr3108_read_button_stat(void);
+uint8_t cy8cmbr3108_read_latched_button_stat(void);
