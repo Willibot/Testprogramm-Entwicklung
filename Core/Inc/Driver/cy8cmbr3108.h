fe08b0300477cdea561027028d1c448ddc1045ea
Willibot <willert@delta-j.de>
2025-07-13 11:12:35 +0000

    Update button status definitions in cy8cmbr3108.h for correct values

--- Core/Inc/Driver/cy8cmbr3108.h
+++ Core/Inc/Driver/cy8cmbr3108.h
@@ -4,8 +4,8 @@
 #include "stm32g0xx_hal.h"
 
 #define CY8CMBR3108_I2C_ADDR            (0x37 << 1) // 8-bit Adresse
-#define CY8CMBR3108_BUTTON_STAT         0x08        // Echtzeit-Tastenstatus (korrekt!)
-#define CY8CMBR3108_LATCHED_BUTTON_STAT 0x00        // Event-Latch (korrekt!)
+#define CY8CMBR3108_BUTTON_STAT         0xAA        // RICHTIG!
+#define CY8CMBR3108_LATCHED_BUTTON_STAT 0xAC        // RICHTIG!
 
 uint8_t cy8cmbr3108_read_latched_button_stat(void);
 uint8_t cy8cmbr3108_read_button_stat(void); // <--- HIER ergänzen!
