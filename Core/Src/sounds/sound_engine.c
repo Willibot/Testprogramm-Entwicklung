15f71c3847a9e8c52a786e1d8651d7312a3f7489
Willibot <willert@delta-j.de>
2025-08-01 16:20:15 +0000

    Refactor sound_engine.c: update sound_beep_start to remove parameters and improve API clarity

--- Core/Src/sounds/sound_engine.c
+++ Core/Src/sounds/sound_engine.c
@@ -6,13 +6,16 @@
 //
 // API:
 //   - sound_engine_set(sound_id_t sound_id, uint8_t repeat): Startet einen Soundeffekt (optional mehrfach)
+//   - sound_engine_play(sound_id_t sound): Startet einen Soundeffekt einmal
 //   - sound_engine_tick(): Muss zyklisch aufgerufen werden, steuert die Soundausgabe
+//   - sound_engine_init(): Initialisiert Piezo-Treiber
 //
 // Hinweise für Entwickler & Copilot:
-//   - Jeder Soundeffekt muss als eigenes Modul eingebunden werden und eine Play-Funktion bereitstellen.
+//   - Jeder Soundeffekt muss als eigenes Modul eingebunden werden und eine Play-/Status-Funktion bereitstellen.
 //   - Erweiterung: Neue Sounds einfach ergänzen (enum, include, switch-case).
 //   - Keine blockierenden Aufrufe! Nur State-Maschinen/Timer in den Sounds.
 //   - Für komplexere Sequenzen: repeat-Logik und Sound-IDs erweitern.
+//   - Die zyklischen Update-Funktionen (sound_beep_update, ...) müssen im Mainloop aufgerufen werden!
 //
 // TODO: Optional: Priorisierung von Sounds, Queue für mehrere Soundereignisse
 // -----------------------------------------------------------------------------
@@ -28,15 +31,25 @@
 static sound_id_t current_sound = SOUND_NONE;
 static uint8_t sound_repeat_counter = 0;
 
+/**
+ * @brief Setzt den aktuellen Soundeffekt und die Wiederholungsanzahl.
+ */
 void sound_engine_set(sound_id_t sound_id, uint8_t repeat) {
     current_sound = sound_id;
     sound_repeat_counter = repeat;
 }
 
+/**
+ * @brief Startet einen Soundeffekt einmal.
+ */
 void sound_engine_play(sound_id_t sound) {
     sound_engine_set(sound, 1);
 }
 
+/**
+ * @brief Zyklische Steuerung der Soundausgabe.
+ * Muss regelmäßig im Mainloop aufgerufen werden!
+ */
 void sound_engine_tick(void) {
     if (current_sound == SOUND_NONE) return;
 
@@ -44,7 +57,7 @@ void sound_engine_tick(void) {
         case SOUND_BEEP:
             // Nur starten, wenn kein Beep aktiv ist!
             if (!sound_beep_is_active()) {
-                sound_beep_start(80, 4000); // Beispiel: 80 ms, 4 kHz
+                sound_beep_start(); // Keine Parameter mehr, fest 4 kHz/80 ms
                 sound_repeat_counter--;
                 if (sound_repeat_counter == 0) {
                     current_sound = SOUND_NONE;
@@ -52,10 +65,10 @@ void sound_engine_tick(void) {
             }
             break;
         case SOUND_DOUBLE_BEEP:
-            // Analog: Hier ggf. sound_double_beep_is_active() prüfen!
+            // Analog: Hier ggf. sound_double_beep_is_active() prüfen und sound_double_beep_start() aufrufen!
             break;
         case SOUND_CONFIG_MODE:
-            // Analog: Hier ggf. sound_config_mode_is_active() prüfen!
+            // Analog: Hier ggf. sound_config_mode_is_active() prüfen und sound_config_mode_start() aufrufen!
             break;
         default:
             break;
@@ -65,5 +78,3 @@ void sound_engine_tick(void) {
 void sound_engine_init(void) {
     piezo_init();
 }
-
-// Hinweis: Die zyklischen Update-Funktionen (sound_beep_update, ...) müssen im Mainloop aufgerufen werden!
